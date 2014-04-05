alex73/ixus160_elph160

commit 94c55cb34cf83a8c254e2a8f01273bdcde6ad7b2

experimental high precision timer for usb remote, from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg111902#msg111902 - not enabled by default
git-svn-id: https://tools.assembla.com/svn/chdk/trunk@3408 6794e30b-3f2a-0410-a806-a2bbca1c07ff

core/usb_remote.c
@@ -37,7 +37,7 @@ int bracketing_timeout = 0 ;
 int usb_power=0;
 static int usb_count=0;
 static int logic_module_usb_count = 0 ;
-int remote_count, remote_space_count, remote_key;
+int remote_mark_count, remote_space_count;
 
 #define USB_BUFFER_SIZE 16
 static int usb_buffer[USB_BUFFER_SIZE] ;
@@ -227,61 +227,80 @@ void usb_remote_status_led(int state)
 
     usb_remote_key()
 
-    - called from the kbd_process() keyboard handler
-    - monitors USB power state and stores in global variable remote_key
+    - called from the kbd_process() keyboard handler or usb_HPtimer_good()
+    - monitors USB power state and stores in global variable remote_state
     - captures the time of 0->1 and 1->0 transitions and buffers them
-    - store most recent power on pulse width in global variable remote_count
-
+    - stores most recent power on pulse width in global variable usb_power
+    - stores most recent pulse count in global variable usb_count
   ---------------------------------------------------------------------------------------------------------*/
 
-
+void usb_buffer_insert(int value)
+{
+    if ( ++usb_buffer_in > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_in = usb_buffer ;    
+    if ( usb_buffer_in == usb_buffer_out )
+    {
+        if ( ++usb_buffer_out > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_out = usb_buffer ;
+    }
+    *usb_buffer_in = value ;
+}
+ 
 void usb_remote_key( void )
 {
     static int pulse_count=0 ;
 
-    remote_key = get_usb_bit() ;
+    usb_state = get_usb_bit() ;
 
     if(conf.remote_enable)
     {
-        if (remote_key) remote_count++ ;                                // track how long the USB power is on
-        else if(remote_space_count<3000) remote_space_count++ ;         // track how long the USB power is off
-
-        if ( remote_space_count > 50 )                                  // pulse counting done if no activity for 500mSec
-        {
-            if( pulse_count > 0 )
+        if (usb_state)
+        {                                                                   // USB power is ON
+            if (remote_mark_count<30000) remote_mark_count++ ;              // track how long the USB power is ON
+            if (remote_space_count != 0)                                    // is this the 0 -> 1 transistion?
+            {                                                               //
+                usb_buffer_insert(remote_space_count);                      // insert space length into buffer
+                remote_space_count = 0 ;                                    // reset the counter
+            }
+        }
+        else
+        {                                                                   // USB power if OFF
+            if(remote_space_count>-30000) remote_space_count-- ;            // track how long the USB power is OFF (note space counts are negative)
+            if (remote_mark_count != 0)                                     // is this the 1 -> 0 transistion?
+            {                                                               //
+                pulse_count++ ;                                             // count pulses transistions
+                usb_power = remote_mark_count;                              // transfer most recent pulse length to variable read by scripts
+                usb_buffer_insert(remote_mark_count);                       // insert pulse length into buffer
+                remote_mark_count = 0;                                      // reset the counter
+            }                                                               //
+            if ((remote_space_count < -50) && (pulse_count > 0))            // pulse counting done if no activity for 50 timer periods
             {
                 usb_count = pulse_count ;
                 logic_module_usb_count = pulse_count ;
                 pulse_count = 0 ;
             }
         }
-
-        if (( remote_key == 0) && (remote_count > 0)  )                                             //  1 -> 0 transistion ?
-        {
-            if( remote_count > 10 ) pulse_count++ ;                                                 // count pulses longer than 100 msec
-
-            usb_power = remote_count;                                                               // transfer most recent pulse length to variable read by scripts
-
-            if ( ++usb_buffer_in > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_in = usb_buffer ;    // insert power pulse width into the buffer
-            if ( usb_buffer_in == usb_buffer_out )
-            {
-                if ( ++usb_buffer_out > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_out = usb_buffer ;
-            }
-            *usb_buffer_in = remote_count ;
-            remote_count = 0;
-        }
-
-        if (( remote_key == 1) && (remote_space_count > 0)  )                                       // 0 -> 1 transistion ?
-        {
-            if ( ++usb_buffer_in > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_in = usb_buffer ;    // insert space pulse width into the buffer as a negative number
-            if ( usb_buffer_in == usb_buffer_out )
-            {
-                if ( ++usb_buffer_out > &usb_buffer[USB_BUFFER_SIZE-1] ) usb_buffer_out = usb_buffer ;
-            }
-            *usb_buffer_in = 0-remote_space_count ;
-            remote_space_count = 0 ;
-        }
     }
+}
+
+
+/*---------------------------------------------------------------------------------------------------------
+
+     High Precision USB Remote Timer Callback routines.
+
+  ---------------------------------------------------------------------------------------------------------*/
+int usb_HPtimer_error_count;
+
+int usb_HPtimer_good(int time, int interval) 
+{
+    usb_HPtimer_handle=0;
+    start_usb_HPtimer(interval) ;
+    usb_remote_key() ;
+    return 0;
+}
+ 
+int usb_HPtimer_bad(int time, int interval) 
+{
+    usb_HPtimer_error_count++;
+    return (usb_HPtimer_good(time, interval));
 }
 
 /*---------------------------------------------------------------------------------------------------------
@@ -316,7 +335,7 @@ int get_usb_power(int mode)
             usb_power = 0;
             break ;
         case USB_STATE :
-            x=remote_key;
+            x=usb_state;
             break ;
         case BUFFERED_PULSE :
             if ( usb_buffer_out != usb_buffer_in )
@@ -332,6 +351,10 @@ int get_usb_power(int mode)
         case LM_PULSE_COUNT :
             x = logic_module_usb_count;
             logic_module_usb_count = 0;
+            break ;
+        case HPTIMER_ERROR_COUNT :
+            x = usb_HPtimer_error_count;
+            usb_HPtimer_error_count = 0;
             break ;
     }
     return x;

core/script.c
@@ -13,7 +13,7 @@
 #include "gui_lang.h"
 #include "ptp.h"
 #include "clock.h"
-
+#include "usb_remote.h"
 #include "script_api.h"
 #include "motion_detector.h"
 
@@ -266,6 +266,9 @@ long script_start_gui( int autostart )
 
     shot_histogram_set(0);
     camera_info.state.auto_started = autostart;
+
+    // Kill high speed USB timer if running
+    stop_usb_HPtimer();
 
     // Keyboard init
     camera_info.state.kbd_last_clicked = 0;

core/usb_remote.h
@@ -15,6 +15,10 @@ extern int get_usb_power(int edge);
 extern void clear_usb_power();
 extern void set_usb_remote_state();
 
+extern int usb_HPtimer_handle;
+extern int start_usb_HPtimer(int);
+extern int stop_usb_HPtimer();
+
 /*===================================================================================================
     Static Variables & #defines
   ===================================================================================================*/
@@ -129,7 +133,8 @@ enum GET_USB_PWR {                          // input parameter for get_usb_power
     USB_STATE ,
     BUFFERED_PULSE ,
     PULSE_COUNT,
-    LM_PULSE_COUNT
+    LM_PULSE_COUNT,
+    HPTIMER_ERROR_COUNT
 } ;
 
 // USB remote processing status

include/camera.h
@@ -40,6 +40,8 @@
     #undef  CAM_ALT_BUTTON_OPTIONS              // Define the list of options for the ALT button - e.g. { KEY_PRINT, KEY_DISPLAY }
     #define CAM_REMOTE                      1   // Camera supports USB-remote
     #undef  CAM_REMOTE_USES_PRECISION_SYNC      // Disable experimental USB remote  precision sync patch
+    #undef  CAM_REMOTE_USB_HIGHSPEED            // Enable highspeed measurements of pulse width & counts on USB port 
+    #define CAM_REMOTE_HIGHSPEED_LIMIT 1000     // Set lowest timer value settable by user
     #undef  GPIO_VSYNC_CURRENT                  // USB remote precision sync : might be 0xC0F070C8 or 0xC0F07008 (http://chdk.setepontos.com/index.php?topic=8312.msg104027#msg104027)
     #undef  CAM_MULTIPART                       // Camera supports SD-card multipartitioning
     #define CAM_HAS_ZOOM_LEVER              1   // Camera has dedicated zoom buttons

lib/ubasic/tokenizer.c
@@ -215,6 +215,7 @@ static const struct keyword_token keywords[] = {
   {"get_autostart",           TOKENIZER_GET_SCRIPT_AUTOSTART},
   {"set_autostart",           TOKENIZER_SET_SCRIPT_AUTOSTART},
   {"get_usb_power",           TOKENIZER_GET_USB_POWER},
+  {"enable_highspeed_usb",    TOKENIZER_ENABLE_HIGHSPEED_USB},
   {"exit_alt",                TOKENIZER_EXIT_ALT},
   {"shut_down",               TOKENIZER_SHUT_DOWN},
 

lib/ubasic/tokenizer.h
@@ -157,6 +157,7 @@ typedef enum {
   TOKENIZER_GET_SCRIPT_AUTOSTART,
   TOKENIZER_SET_SCRIPT_AUTOSTART,
   TOKENIZER_GET_USB_POWER,
+  TOKENIZER_ENABLE_HIGHSPEED_USB,
   TOKENIZER_EXIT_ALT,
   TOKENIZER_SHUT_DOWN,
   TOKENIZER_SET_ND_FILTER,

lib/ubasic/ubasic.c
@@ -679,6 +679,12 @@ static int factor(void)
     accept(TOKENIZER_GET_DRAW_TITLE_LINE);  
     r = camera_info.state.osd_title_line ;
     break;
+  case TOKENIZER_ENABLE_HIGHSPEED_USB:
+    accept(TOKENIZER_ENABLE_HIGHSPEED_USB);
+    int hpenable= expr();
+    if ( hpenable > 0) r = start_usb_HPtimer(hpenable);
+    else r = stop_usb_HPtimer();
+    break;
 
   //ARM Begin
       

modules/luascript.c
@@ -1266,6 +1266,17 @@ static int luaCB_get_usb_power( lua_State* L )
   return 1;
 }
 
+// enable USB High Perfomance timer
+static int luaCB_enable_highspeed_usb( lua_State* L )
+{
+  int val= luaL_checknumber(L,1);
+  if (val > 0 )
+     val=start_usb_HPtimer(val);
+  else  val=stop_usb_HPtimer() ;
+  lua_pushnumber(L, val);
+  return 1;
+}
+
 static int luaCB_enter_alt( lua_State* L )
 {
   enter_alt();
@@ -2688,6 +2699,7 @@ static const luaL_Reg chdk_funcs[] = {
     FUNC(get_autostart)
     FUNC(set_autostart)
     FUNC(get_usb_power)
+    FUNC(enable_highspeed_usb)
     FUNC(enter_alt)
     FUNC(exit_alt)
     FUNC(shut_down)

commit 342a2ad450a57c69fb07380b75cc36f478330b4d

fix for old cameras broken in r3408 from waterwingz in http://chdk.setepontos.com/index.php?topic=650.msg111915#msg111915
git-svn-id: https://tools.assembla.com/svn/chdk/trunk@3410 6794e30b-3f2a-0410-a806-a2bbca1c07ff

core/usb_remote.c
@@ -126,7 +126,7 @@ static void debug_add_rec()
     if (!debug_rec_buf) return;
 
     debug_rec_buf[debug_num_rec].tick = get_tick_count();
-    debug_rec_buf[debug_num_rec].usb = remote_key;
+    debug_rec_buf[debug_num_rec].usb = usb_state;
 
     debug_rec_buf[debug_num_rec].driver_state = driver_state;
     debug_rec_buf[debug_num_rec].virtual_remote_state = virtual_remote_state;

platform/a1200/kbd.c
@@ -17,7 +17,6 @@ static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 static long alt_mode_key_mask = 0x00000080;
 
 static int usb_power=0;
-static int remote_key, remote_count;
 static int shoot_counter=0;
 
 extern void _GetKbdState(long*);

platform/a800/kbd.c
@@ -34,7 +34,6 @@ static long kbd_prev_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 static long kbd_mod_state[3] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 
 static int usb_power=0;
-static int remote_key, remote_count;
 static int shoot_counter=0;
 extern void _platformsub_kbd_fetch_data(long*);
 

platform/ixus30_sd200/kbd.c
@@ -53,8 +53,8 @@ static int kbd_data_process_request_data=0;
 
 #define USB_MASK 0x40 
 #define USB_IDX  2
-extern int remote_key ;
-extern int remote_count ;
+extern int usb_state ;
+extern int remote_mark_count ;
 extern int usb_power ;
 
 int get_usb_bit() 
@@ -617,12 +617,12 @@ int usb_power_status_override(int status){
  // for get USB power flag read status & USB_MASK
 	
 	if (conf.remote_enable) {
-		remote_key = (status & USB_MASK)==USB_MASK;
-		if (remote_key) 
-			remote_count += 1;
-		else if (remote_count) {
-			usb_power = remote_count;
-			remote_count = 0;
+		usb_state = (status & USB_MASK)==USB_MASK;
+		if (usb_state) 
+			remote_mark_count += 1;
+		else if (remote_mark_count) {
+			usb_power = remote_mark_count;
+			remote_mark_count = 0;
 		}
 		return status &~USB_MASK;
 	}

platform/ixus40_sd300/kbd.c
@@ -54,8 +54,8 @@ static int kbd_data_process_request_data=0;
 
 #define USB_MASK 0x40 
 #define USB_IDX  2
-extern int remote_key ;
-extern int remote_count ;
+extern int usb_state ;
+extern int remote_mark_count ;
 extern int usb_power ;
 int get_usb_bit() 
 {
@@ -631,12 +631,12 @@ int usb_power_status_override(int status){
  // for get USB power flag read status & USB_MASK
 	
 	if (conf.remote_enable) {
-		remote_key = (status & USB_MASK)==USB_MASK;
-		if (remote_key) 
-			remote_count += 1;
-		else if (remote_count) {
-			usb_power = remote_count;
-			remote_count = 0;
+		usb_state = (status & USB_MASK)==USB_MASK;
+		if (usb_state) 
+			remote_mark_count += 1;
+		else if (remote_mark_count) {
+			usb_power = remote_mark_count;
+			remote_mark_count = 0;
 		}
 		return status &~USB_MASK;
 	}

platform/ixus50_sd400/kbd.c
@@ -21,8 +21,8 @@ static KeyMap keymap[];
 
 #define USB_MASK 0x40 
 #define USB_IDX 2
-extern int remote_key ;
-extern int remote_count ;
+extern int usb_state ;
+extern int remote_mark_count ;
 extern int usb_power ;
 int get_usb_bit() 
 {
@@ -220,12 +220,12 @@ int usb_power_status_override(int status){
  // for clear USB power flag  - return status &~USB_MASK;
  // for get USB power flag read status & USB_MASK
 	
-		remote_key = (status & USB_MASK)==USB_MASK;
-		if (remote_key) 
-			remote_count += 1;
-		else if (remote_count) {
-			usb_power = remote_count;
-			remote_count = 0;
+		usb_state = (status & USB_MASK)==USB_MASK;
+		if (usb_state) 
+			remote_mark_count += 1;
+		else if (remote_mark_count) {
+			usb_power = remote_mark_count;
+			remote_mark_count = 0;
 		}
 	if (conf.remote_enable) {
 		return status &~USB_MASK;

platform/ixus700_sd500/kbd.c
@@ -21,8 +21,8 @@ static KeyMap keymap[];
 
 #define USB_MASK 0x40 
 #define USB_IDX 2
-extern int remote_key ;
-extern int remote_count ;
+extern int usb_state ;
+extern int remote_mark_count ;
 extern int usb_power ;
 int get_usb_bit() 
 {
@@ -222,12 +222,12 @@ int usb_power_status_override(int status){
  // for clear USB power flag  - return status &~USB_MASK;
  // for get USB power flag read status & USB_MASK
 	
-		remote_key = (status & USB_MASK)==USB_MASK;
-		if (remote_key) 
-			remote_count += 1;
-		else if (remote_count) {
-			usb_power = remote_count;
-			remote_count = 0;
+		usb_state = (status & USB_MASK)==USB_MASK;
+		if (usb_state) 
+			remote_mark_count += 1;
+		else if (remote_mark_count) {
+			usb_power = remote_mark_count;
+			remote_mark_count = 0;
 		}
 	if (conf.remote_enable) {
 		return status &~USB_MASK;

platform/ixusizoom_sd30/kbd.c
@@ -23,8 +23,6 @@ static KeyMap keymap[];
 
 #define USB_MASK 0x40 	// not implemented in this camera
 #define USB_IDX 2
-extern int remote_key ;
-extern int remote_count ;
 extern int usb_power ;
 int get_usb_bit() 
 {