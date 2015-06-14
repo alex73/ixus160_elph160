d182bd373338436fb4095e8514642acc67d2bf53
Anthony Dong <aanthony.dong@gmail.com>
2015-06-14 12:00:19 +0200

    raw/dng defines

--- platform/ixus160_elph160/platform_camera.h
+++ platform/ixus160_elph160/platform_camera.h
@@ -9,6 +9,14 @@
 #define PARAM_CAMERA_NAME 3 // Found @0xffcb1568
 #define MKDIR_RETURN_ONE_ON_SUCCESS
 
+#define CAM_JPEG_WIDTH                  5152
+#define CAM_JPEG_HEIGHT                 3864
+#define CAM_ACTIVE_AREA_X1              48      //To do
+#define CAM_ACTIVE_AREA_Y1              16      //To do
+#define CAM_ACTIVE_AREA_X2              5200    //To do
+#define CAM_ACTIVE_AREA_Y2              3892    //To do
+
+
 // Guessed
 #define CAM_PROPSET	6
 
@@ -24,8 +32,17 @@
 #define CAM_USES_ASPECT_CORRECTION 1
 #define CAM_BITMAP_WIDTH 720
 #define CAM_BITMAP_HEIGHT 360
-    #undef CAM_SENSOR_BITS_PER_PIXEL
-    #define CAM_SENSOR_BITS_PER_PIXEL       12
+#undef CAM_SENSOR_BITS_PER_PIXEL
+#define CAM_SENSOR_BITS_PER_PIXEL       12
+
+#define cam_CFAPattern 0x02010100 // Red  Green  Green  Blue
+
+    //TODO
+    #define CAM_COLORMATRIX1                             \
+    827547, 1000000, -290458, 1000000, -126086, 1000000, \
+    -12829, 1000000,  530507, 1000000,   50537, 1000000, \
+      5181, 1000000,   48183, 1000000,  245014, 1000000
+    #define cam_CalibrationIlluminant1      1       // Daylight
 
 //#define DRAW_ON_ACTIVE_BITMAP_BUFFER_ONLY
 
